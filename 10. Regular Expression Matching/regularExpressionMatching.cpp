80e768d2177d40095e652609b5babcfc08edf8cd
Flyer Cheng <CFX674939613@gmail.com>
2017-02-21 21:50:56 +0800

    question10

--- 10. Regular Expression Matching/regularExpressionMatching.cpp
+++ 10. Regular Expression Matching/regularExpressionMatching.cpp
@@ -5,25 +5,31 @@ using namespace std;
 void regularExpressionMatching::example()
 {
 	regularExpressionMatching test;
-	cout << test.isMatch(string("aa"), string("a")) << endl;
+	cout << boolalpha <<test.isMatch(string(""), string("")) << endl;
 }
 
 bool regularExpressionMatching::isMatch(string s, string p)
 {
-	//judging if string is empty
-	if (s.empty() || p.empty())
-		return false;
-	int sIndex = 0, sSize = s.size();
-	int pIndex = 0, pSize = p.size();
-	int matchStartPosIndex = 0;
-	for (; matchStartPosIndex < pSize; ++matchStartPosIndex)
+	int i = 0, j = 0;
+	for (;(i < s.size()) && (j < p.size()); ++i, ++j )
 	{
-
+		//Judging if s[i] is matching with p[j]
+		if (s[i] == p[j] || p[j] == '.') continue;
+		//s[i] isn't matching with p[j]
+		//Judging if p[j] == '*'and if p[j+1] == '*'  
+		if (p[j] != '*')
+		{
+			int temp = j + 1;
+			if (temp >= p.size() || p[temp] != '*') return false;
+			++j;
+			continue;
+		}
+		// p[j] == '*'
+		if (p[j - 1] == '.') return true;
+		if (s[i] != p[j - 1]) return false;
+		++i;
+		while (i < s.size() && s[i] == p[j - 1])++i;	
 	}
-	//while (sIndex < sSize)
-	//{
-	//	char tempChar = s[sIndex];
-	//	
-	//}
-	return true;
+	if (p[j] == '*')++j;
+	return ((i >= s.size()) && (j >= p.size())) ? true : false;
 }
