9a13b658a5df257407f06d25660b8bda2d42bdd9
Flyer Cheng <CFX674939613@gmail.com>
2017-02-24 22:46:00 +0800

    question10

--- 10. Regular Expression Matching/regularExpressionMatching.cpp
+++ 10. Regular Expression Matching/regularExpressionMatching.cpp
@@ -1,6 +1,7 @@
 #include"regularExpressionMatching.h"
 #include<iostream>
 #include<vector>
+#include<exception>
 
 using namespace std;
 
@@ -13,12 +14,16 @@ void regularExpressionMatching::example()
 bool regularExpressionMatching::isMatch(string s, string p)
 {
 	//Judging parameters
+	if (!p.empty() && (p[0] == '*'))
+		throw exception("the first letter of regular expression is *");
 	if (s.empty())
 	{
 		if (p.empty())
 			return true;
 		else if ((p.size() > 2) || ((p.size() == 2) && (p[1] != '*')))
 			return false;
+		else if ((p.size() == 2) && (p[1] == '*'))
+			return true;
 	}
 	if (p.empty())
 		return false;
