dfa1d982e45567af4593c7403980d900a2afa864
Flyer Cheng <CFX674939613@gmail.com>
2017-02-24 22:39:09 +0800

    question10

--- 10. Regular Expression Matching/regularExpressionMatching.cpp
+++ 10. Regular Expression Matching/regularExpressionMatching.cpp
@@ -12,5 +12,16 @@ void regularExpressionMatching::example()
 
 bool regularExpressionMatching::isMatch(string s, string p)
 {
+	//Judging parameters
+	if (s.empty())
+	{
+		if (p.empty())
+			return true;
+		else if ((p.size() > 2) || ((p.size() == 2) && (p[1] != '*')))
+			return false;
+	}
+	if (p.empty())
+		return false;
+
 	
 }
