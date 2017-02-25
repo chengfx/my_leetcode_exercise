9154dab70a0bd4cc7fbc34f89f71e61f9642d7a2
Flyer Cheng <CFX674939613@gmail.com>
2017-02-25 16:46:33 +0800

    question10

--- 10. Regular Expression Matching/regularExpressionMatching.cpp
+++ 10. Regular Expression Matching/regularExpressionMatching.cpp
@@ -9,7 +9,7 @@ using namespace std;
 void regularExpressionMatching::example()
 {
 	regularExpressionMatching test;
-	cout << boolalpha <<test.isMatch(string("abbabaaaaaaacaa"), string("a*.*b.a.*c*b*a*c*")) << endl;
+	cout << boolalpha <<test.DP(string("aa"), string("a*")) << endl;
 
 }
 
@@ -91,4 +91,24 @@ bool regularExpressionMatching::isMatch(string s, string p)
 		}
 	}
 	return false;
+}
+
+bool regularExpressionMatching::DP(string s, string p)
+{
+	int m = s.size(), n = p.size();
+	vector<vector<bool>> table(m + 1, vector<bool>(n + 1, false));
+	table[0][0] = true;
+	// if s is empty
+	for (int i = 2; i <= n; i++)
+		table[0][i] = (p[i - 1] == '*') && (table[0][i - 2]);
+	for(int i = 1;i<=m;i++)
+		for (int j = 1; j <= n; j++)
+		{
+			int preJ = j - 1, preI = i - 1;
+			if (p[preJ] != '*')
+				table[i][j] = table[preI][preJ] && ((s[preI] == p[preJ]) || (p[preJ] == '.'));
+			else
+				table[i][j] = table[i][j - 2] || (table[preI][j] && ((s[preI] == p[j - 2]) || (p[j - 2] == '.')));
+		}
+	return table[m][n];
 }
--- 10. Regular Expression Matching/regularExpressionMatching.h
+++ 10. Regular Expression Matching/regularExpressionMatching.h
@@ -6,4 +6,5 @@ class regularExpressionMatching {
 public:
 	void example();
 	bool isMatch(std::string s, std::string p);
+	bool DP(std::string s, std::string p);
 };
