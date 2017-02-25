41223bed7dddbb163e12e2ba022b330062fc3617
Flyer Cheng <CFX674939613@gmail.com>
2017-02-25 13:07:57 +0800

    question10

--- 10. Regular Expression Matching/regularExpressionMatching.cpp
+++ 10. Regular Expression Matching/regularExpressionMatching.cpp
@@ -8,7 +8,8 @@ using namespace std;
 void regularExpressionMatching::example()
 {
 	regularExpressionMatching test;
-	cout << boolalpha <<test.isMatch(string(""), string("")) << endl;
+	cout << boolalpha <<test.isMatch(string("aaa"), string("ab*a*c*a*a*a")) << endl;
+
 }
 
 bool regularExpressionMatching::isMatch(string s, string p)
@@ -27,7 +28,6 @@ bool regularExpressionMatching::isMatch(string s, string p)
 	}
 	if (p.empty())
 		return false;
-
 	//Starting to check if it's matching
 	vector<int> index(1,0);
 	for (int i = 0; i < s.size(); i++)
@@ -39,18 +39,47 @@ bool regularExpressionMatching::isMatch(string s, string p)
 			if (temp >= p.size())
 				continue;
 			//check
+			int lastTemp = temp + 1;
 			if ((s[i] == p[temp]) || (p[temp] == '.'))
-				tempIndex.push_back(temp + 1);
+				tempIndex.push_back(lastTemp);
+			if ((lastTemp < p.size()) && (p[lastTemp] == '*'))
+				++temp;
+			lastTemp = temp + 1;
 			if (p[temp] == '*')
 			{
 				int preTemp = temp - 1;
+				//Judging continous '*'
+				if ((p[preTemp] == '*') || ((lastTemp < p.size()) && (p[lastTemp] == '*')))
+					throw exception("continous *");
 				if (p[preTemp] == s[i] || (p[preTemp] == '.'))
 					tempIndex.push_back(temp);
-				else if (p[preTemp] == '*')
-					throw exception(" two '*'");
+				int k = lastTemp;
+				while (k < p.size())
+				{
+					if ((s[i] == p[k]) || (p[k] == '.'))
+						tempIndex.push_back(k + 1);
+					++k;
+					if ((k < p.size()) && (p[k] == '*'))
+						++k;
+					else
+						break;
+				}
 			}
 		}
+		//Matching is fail
+		if (tempIndex.empty())
+			return false;
 		index = tempIndex;
 	}
-	
+	for (auto& temp : index)
+	{
+		int lastTemp = temp + 1;
+		if (temp >= p.size())
+			return true;
+		if ((lastTemp == p.size()) && (p[temp] == '*'))
+			return true;
+		if (((temp + 2) == p.size()) && (p[lastTemp] == '*'))
+			return true;
+	}
+	return false;
 }
