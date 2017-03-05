4207ac561da059aa9d5cc1257f68ac299b1e47fd
Flyer Cheng <CFX674939613@gmail.com>
2017-03-05 20:00:19 +0800

    question

--- 4.MedianofTwoSortedArrays/MediumOfArrays.cpp
+++ 4.MedianofTwoSortedArrays/MediumOfArrays.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 void MediumOfArrays::example()
 {
-	vector<int> nums1 = { 2 };
-	vector<int> nums2 = { };
+	vector<int> nums1 = {1,3,5,6,9,11 };
+	vector<int> nums2 = {2,4,7,8,10,12 };
+	int a = 0, b = 0;
 	cout << "result is :" << findMedianSortedArrays(nums1, nums2) << endl;
+	GetKthNumber(nums1, &a, nums2, &b, 11);
 }
 double MediumOfArrays::findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
 {
@@ -73,7 +75,49 @@ double MediumOfArrays::findMedianSortedArrays(vector<int>& nums1, vector<int>& n
 	return 0;
 }
 
-bool MediumOfArrays::GetKthNumber(vector<int>& nums1, int* nums1Index, vector<int>& nums2, int* nums2Index)
+bool MediumOfArrays::GetKthNumber(vector<int>& nums1, int* nums1Index, vector<int>& nums2, int* nums2Index,int kth)
 {
-
+	int nums1Length = nums1.size(), nums2Length = nums2.size();
+	//critical condition
+	if (((nums1Length == 0) && (nums2Length == 0)) || ((nums1Length + nums2Length) < kth))
+	{
+		*nums1Index = -1; *nums2Index = -1;
+		return false;
+	}
+	else if (nums1Length == 0)
+	{
+		*nums1Index = -1; *nums2Index = kth;
+		return false;
+	}
+	else if (nums2Length == 0)
+	{
+		*nums2Index = -1; *nums1Index = kth;
+		return false;
+	}
+	//binary search
+	int n1Start = 0, n1End = nums1Length - 1, n2Start = 0, n2End = nums2Length - 1;
+	int n1pos = 0, n2Pos = 0;
+	while ((n1Start < n1End) && (n2Start < n2End))
+	{
+		int n1Mid = (n1Start + n1End) >> 1, n2Mid = (n2Start + n2End) >> 1;
+		int num = n1Mid + n2Mid + 2;
+		if (kth < num)
+		{
+			//the array which has bigger number needs to change its  End index
+			if (nums1[n1Mid] <= nums2[n2Mid]) 
+				n2End = n2Mid - 1;
+			else
+				n1End = n1Mid - 1;
+		}
+		else
+		{
+			//the array which has smaller number needs to change its start index
+			if (nums1[n1Mid] <= nums2[n2Mid])
+				n1Start = n1Mid + 1;
+			else
+				n2Start = n2Mid + 1;
+		}
+		n1pos = n1Mid; n2Pos = n2Mid;
+	}
+	cout << "n1pos:" << n1pos << " n2pos:" << n2Pos << endl;
 }
--- 4.MedianofTwoSortedArrays/MediumOfArrays.h
+++ 4.MedianofTwoSortedArrays/MediumOfArrays.h
@@ -6,5 +6,5 @@ class MediumOfArrays
 public:
 	void example();
 	double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2);
-	bool GetKthNumber(std::vector<int>& nums1, int* nums1Index, std::vector<int>& nums2, int* nums2Index);
+	bool GetKthNumber(std::vector<int>& nums1, int* nums1Index, std::vector<int>& nums2, int* nums2Index, int kth);
 };
